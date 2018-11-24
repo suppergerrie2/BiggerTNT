f559e8df8b47ccd7e4b2828c23ae65394fb94e24
suppergerrie2 <gerjan2001@gmail.com>
2018-11-24 14:56:38 +0100

    Comments

--- src/main/java/com/suppergerrie2/biggertnt/entities/rendering/RenderEntityBiggerTNT.java
+++ src/main/java/com/suppergerrie2/biggertnt/entities/rendering/RenderEntityBiggerTNT.java
@@ -21,16 +21,23 @@ public class RenderEntityBiggerTNT extends Render<EntityBiggerTNT> {
 
 	@Override
 	protected ResourceLocation getEntityTexture(EntityBiggerTNT entity) {
+		//The tnt uses a block texture, so we return the location where all of the block textures are.
         return TextureMap.LOCATION_BLOCKS_TEXTURE;
 	}
 
 	@Override
 	public void doRender(EntityBiggerTNT entity, double x, double y, double z, float entityYaw, float partialTicks)
 	{
+		//The blockRenderDispatcher is used to render a block
 		BlockRendererDispatcher blockrendererdispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
+		
 		GlStateManager.pushMatrix();
+		
+		//Make sure we render at the correct location
 		GlStateManager.translate((float)x, (float)y + 0.5F, (float)z);
 
+		//If the tnt is almost exploding we will change the size of the rendered cube
+		//This makes it look like it is expanding just before exploding
 		if ((float)entity.getFuse() - partialTicks + 1.0F < 10.0F)
 		{
 			//Just before the tnt explodes it grows in size, this line calculates how close to explosion the tnt is (it will be 1 when it explodes)
@@ -45,12 +52,23 @@ public class RenderEntityBiggerTNT extends Render<EntityBiggerTNT> {
 		}
 
 		float alpha = (1.0F - ((float)entity.getFuse() - partialTicks + 1.0F) / 100.0F) * 0.8F;
+		
+		//Tell the game we want to use the texture we specified in getEntityTexture
 		this.bindEntityTexture(entity);
+		
+		//Some rotating and translating, probably to get it at the middle of the block facing the correct way
 		GlStateManager.rotate(-90.0F, 0.0F, 1.0F, 0.0F);
 		GlStateManager.translate(-0.5F, -0.5F, 0.5F);
+		
+		//Here the rendering happens! The blockRenderDispatcher will render the state we pass in with the brightness.
+		//The state we use is the default state for the explosion block
+		//The brightness is based on the entity brightness
 		blockrendererdispatcher.renderBlockBrightness(ModBlocks.explosionBlock.getDefaultState(), entity.getBrightness());
+		
+		//Seems like the renderBlockBrightness changes where we are so we need to translate back
 		GlStateManager.translate(0.0F, 0.0F, 1.0F);
 
+		//Idk what renderOutlines is used for but vanilla used it
 		if (this.renderOutlines)
 		{
 			GlStateManager.enableColorMaterial();
@@ -59,7 +77,7 @@ public class RenderEntityBiggerTNT extends Render<EntityBiggerTNT> {
 			GlStateManager.disableOutlineMode();
 			GlStateManager.disableColorMaterial();
 		}
-		//Color blinking
+		//This makes the block blink based on the we first divide by 5 to slow the blinking down.
 		else if (entity.getFuse() / 5 % 2 == 0)
 		{
 			GlStateManager.disableTexture2D();
@@ -69,7 +87,9 @@ public class RenderEntityBiggerTNT extends Render<EntityBiggerTNT> {
 			GlStateManager.color(1.0F, 1.0F, 1.0F, alpha);
 			GlStateManager.doPolygonOffset(-3.0F, -3.0F);
 			GlStateManager.enablePolygonOffset();
+			
 			blockrendererdispatcher.renderBlockBrightness(ModBlocks.explosionBlock.getDefaultState(), 1.0F);
+			
 			GlStateManager.doPolygonOffset(0.0F, 0.0F);
 			GlStateManager.disablePolygonOffset();
 			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
